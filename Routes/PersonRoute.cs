fee8c7e38a470e8700bc5a37011d3d0e0820c4a2
Lucas Salomao <70116782@amcontratos.com.br>
2024-10-20 15:26:53 -0300

    feat: new endpoint for get single by Id

--- Routes/PersonRoute.cs
+++ Routes/PersonRoute.cs
@@ -16,34 +16,47 @@ namespace Person.Routes
                 var people = await context.People.ToListAsync();
                 return Results.Ok(people);
             });
+            
+            route.MapGet("{id:guid}", async (Guid id, PersonContext context) =>
+            {
+                var person = await context.People.FindAsync(id);
+
+                return person == null ? Results.NotFound() : Results.Ok(person);
+            });
 
             route.MapPost("", async (PersonDTO dto, PersonContext context) =>
             {
                 var personEntity = new PersonModel(dto.name);
+
                 await context.People.AddAsync(personEntity);
                 await context.SaveChangesAsync();
-                return Results.Created($"/person/{personEntity.Id}", personEntity); // Retorna o recurso criado
+
+                return Results.Created($"/person/{personEntity.Id}", personEntity);
             });
 
             route.MapDelete("{id:guid}", async (Guid id, PersonContext context) =>
             {
                 var person = await context.People.FindAsync(id);
+
                 if (person == null)
                     return Results.NotFound();
 
                 context.People.Remove(person);
                 await context.SaveChangesAsync();
+
                 return Results.NoContent();
             });
 
             route.MapPut("{id:guid}", async (Guid id, PersonDTO dto, PersonContext context) =>
             {
                 var person = await context.People.FindAsync(id);
+
                 if (person == null)
                     return Results.NotFound();
 
                 person.UpdateName(dto.name);
                 await context.SaveChangesAsync();
+
                 return Results.Ok(person);
             });
         }
